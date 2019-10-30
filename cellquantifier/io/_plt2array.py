12487e30289c1767b7fd376d8101e9296b353541
hualin0123 <hualin_0123@hotmail.com>
2019-10-30 15:59:14 -0400

    add plt2array()

---
+++ cellquantifier/io/_plt2array.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plt2array(fig):
+    """
+    ------------------------------------------------
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from nubo.io import plt2rgbndarray
+
+    t = np.linspace(0, 4*np.pi, 1000)
+    fig, ax = plt.subplots()
+    ax.plot(t, np.cos(t))
+    ax.plot(t, np.sin(t))
+    out = plt2rgbndarray(fig)
+    print(out)
+    ------------------------------------------------
+    """
+
+    """
+    Save matplotlib.pyplot figure to numpy rgbndarray.
+
+    Parameters
+    ----------
+    fig : object
+        matplotlib figure object.
+
+    Returns
+    -------
+    rgb_array_3d: ndarray
+        3d ndarray represting the figure
+
+    Examples
+    --------
+    >>> import matplotlib.pyplot as plt
+    >>> import numpy as np
+    >>> from cellquantifier.io._plt2array import plt2array
+    >>> t = np.linspace(0, 4*np.pi, 1000)
+    >>> fig, ax = plt.subplots()
+    >>> ax.plot(t, np.cos(t))
+    >>> ax.plot(t, np.sin(t))
+    >>> result_array_3d = plt2array(fig)
+    >>> print(result_array_3d.shape)
+    (480, 640, 3)
+    """
+
+    fig.canvas.draw()
+    buf = fig.canvas.tostring_rgb()
+    ncols, nrows = fig.canvas.get_width_height()
+    rgb_array_3d = np.frombuffer(buf, dtype=np.uint8).reshape(nrows, ncols, 3)
+
+    return rgb_array_3d
