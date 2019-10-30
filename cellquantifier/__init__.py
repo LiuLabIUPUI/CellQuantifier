8694e1dd85283e68910bbc951da3b9eead58051c
hualin0123 <hualin_0123@hotmail.com>
2019-10-30 13:40:26 -0400

    add __init__ file for cellquantifier

---
+++ cellquantifier/__init__.py
@@ -0,0 +1,44 @@
+"""CellQuantifier
+
+``CellQuantifier`` (a.k.a. ``cq``) is a collection of algorithms to get
+quantitative information of living cells.
+
+Environment requirements
+------------------------
+conda install -c conda-forge trackpy
+conda install -c conda-forge pims
+conda install numba
+conda install -c conda-forge scikit-image
+conda install -c bioconda centrosome
+conda install seaborn
+conda install matplotlib_scalebar
+
+Subpackages
+-----------
+    Category 1
+    ----------
+    segm
+        Cell segmentation.
+    regi
+        Cell registration.
+    deno
+        Denoise.
+    smt
+        Single molecule tracking.
+
+    Category 2
+    ----------
+    io
+        Input and output.
+    data
+        Data.
+    math
+        Math.
+    plot
+        Plot.
+
+    Category 3
+    ----------
+    util
+        Utility.
+"""
