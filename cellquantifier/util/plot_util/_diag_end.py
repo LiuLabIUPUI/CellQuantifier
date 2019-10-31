8465d52acf12c05a5bbf887f294e92b60ebd6e13
hualin0123 <hualin_0123@hotmail.com>
2019-10-31 09:54:18 -0400

    add diag_end()

---
+++ cellquantifier/util/plot_util/_diag_end.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from cellquantifier.io._plt2array import plt2array
+
+def diag_end(fig, pltshow=False):
+    """
+    Typical piece of code at the end of diagnostic.
+
+    Parameters
+    ----------
+    fig : object
+        matplotlib figure object.
+    pltshow: bool, optional
+        If True, show the fig.
+        If False, clear and close the fig.
+
+    Returns
+    -------
+    rgb_array_3d: ndarray
+        3d ndarray represting the figure
+    """
+
+    rgb_array_3d = plt2array(fig)
+    if pltshow:
+        plt.show()
+    else:
+        plt.clf()
+        plt.close()
+    return rgb_array_3d
