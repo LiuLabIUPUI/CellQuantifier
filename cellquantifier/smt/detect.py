f5777e7aac3dac0080cfc38b59704bc25c52f072
hualin0123 <hualin_0123@hotmail.com>
2019-11-01 16:28:13 -0400

    optimized detect.py and fit_psf.py

--- cellquantifier/smt/detect.py
+++ cellquantifier/smt/detect.py
@@ -1,105 +1,262 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
+import numpy as np; import pandas as pd; import pims
+from cellquantifier.util.plot_util.anno import anno_blob, anno_scatter
+from cellquantifier.util.plot_util._plot_end import plot_end
 from matplotlib_scalebar.scalebar import ScaleBar
 from skimage.feature import blob_log
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
-def detect_blobs(image,
-		 min_sigma = 1,
-		 max_sigma = 3,
-		 num_sigma = 5,
-		 threshold = .5,
-		 peak_threshold_rel = .1,
-		 r_to_sigraw = 3,
-		 scatter_detection=False):
-
+def detect_blobs(pims_frame,
+				min_sig=1,
+				max_sig=3,
+				num_sig=5,
+				blob_thres=0.1,
+				peak_thres_rel=0.1,
+				r_to_sigraw=3,
+				pixel_size = 108.4,
+				diagnostic=True,
+				pltshow=True,
+				plot_r=True,
+				truth_df=None):
 	"""
-	Detect blobs in time-lapse images
+    Detect blobs for each frame.
 
-	Parameters
-	----------
-	image : 2D/3D ndarray
-		raw image data
-	Returns
-	-------
-	blobs_df : DataFrame object
+    Parameters
+    ----------
+    pims_frame : pims.Frame object
+        Each frame in the format of pims.Frame.
+    min_sig : float, optional
+        As 'min_sigma' argument for blob_log().
+	max_sig : float, optional
+        As 'max_sigma' argument for blob_log().
+    num_sig : int, optional
+        As 'num_sigma' argument for blob_log().
+	blob_thres : float, optional
+        As 'threshold' argument for blob_log().
+	peak_thres_rel : float, optional
+        Relative peak threshold [0,1].
+		Blobs below this relative value are removed.
+	r_to_sigraw : float, optional
+        Multiplier to sigraw to decide the fitting patch radius.
+	pixel_size : float, optional
+		Pixel size in nm. Used for the scale bar.
+	diagnostic : bool, optional
+        If true, run the diagnostic.
+    pltshow : bool, optional
+        If true, show diagnostic plot.
+    plot_r : bool, optional
+		If True, plot the blob boundary.
+	truth_df : DataFrame or None. optional
+		If provided, plot the ground truth position of the blob.
 
-	Examples
-	--------
-	>>> from cellquantifier import data
-	>>> from cellquantifier.smt.detect import detect_blobs
-	>>> detect_blobs(data.simulated_cell(), threshold=.1)
+    Returns
+    -------
+    blobs_df : DataFrame
+        columns = ['frame', 'x', 'y', 'sig_raw', 'r',
+					'peak', 'mass', 'mean', 'std']
+    plt_array :  ndarray
+        ndarray of diagnostic plot.
 
-	         x      y  r
-	0    286.0  361.0        7.5
-	1    286.0  292.0        4.5
-	2    286.0  246.0        3.0
-	3    286.0   46.0        4.5
-	4    285.0  349.0        6.0
-	..     ...    ...        ...
-	620   20.0  349.0        3.0
-	621   19.0  336.0        9.0
-	622   18.0  346.0        3.0
-	623   18.0  343.0        3.0
-	624   18.0  331.0        4.5
+    Examples
+    --------
+	import pims
+	from cellquantifier.smt.detect import detect_blobs, detect_blobs_batch
+	frames = pims.open('cellquantifier/data/simulated_cell.tif')
+	detect_blobs(frames[0])
+    """
 
-		"""
+	# """
+    # ~~~~~~~~~~~~~~~~~Detection using skimage.feature.blob_log~~~~~~~~~~~~~~~~~
+    # """
 
-	blobs_df = pd.DataFrame(columns = ['frame', 'x', 'y', 'r'])
+	frame = pims_frame
+	blobs = blob_log(frame,
+	                 min_sigma=min_sig,
+	                 max_sigma=max_sig,
+	                 num_sigma=num_sig,
+	                 threshold=blob_thres)
 
-	for i in range(len(image[:])):
+	# """
+    # ~~~~~~~~~~~~~~~~~~~~~~Prepare blobs_df and update it~~~~~~~~~~~~~~~~~~~~~~
+    # """
 
-		raw_blobs = blob_log(image[i],
-				 min_sigma = min_sigma,
-				 max_sigma = max_sigma,
-				 num_sigma = num_sigma,
-				 threshold = threshold)
+	columns = ['frame', 'x', 'y', 'sig_raw', 'r', 'peak', 'mass', 'mean', 'std']
+	blobs_df = pd.DataFrame([], columns=columns)
+	blobs_df['x'] = blobs[:, 0]
+	blobs_df['y'] = blobs[:, 1]
+	blobs_df['sig_raw'] = blobs[:, 2]
+	blobs_df['r'] = blobs[:, 2] * r_to_sigraw
+	blobs_df['frame'] = pims_frame.frame_no
+	for i in blobs_df.index:
+	    x = int(blobs_df.at[i, 'x'])
+	    y = int(blobs_df.at[i, 'y'])
+	    r = int(round(blobs_df.at[i, 'r']))
+	    blob = frame[x-r:x+r+1, y-r:y+r+1]
+	    blobs_df.at[i, 'mass'] = blob.sum()
+	    blobs_df.at[i, 'std'] = blob.std()
+	    blobs_df.at[i, 'peak'] = blob.max()
+	    blobs_df.at[i, 'mean'] = blob.mean()
 
-		df = pd.DataFrame({'frame': i*np.ones((raw_blobs.shape[0],)), 'x': raw_blobs[:, 0], 'y': raw_blobs[:, 1], 'r': raw_blobs[:, 2]})
-		df_np = df.to_numpy()
+	# """
+    # ~~~~~~~Filter detections at the edge and those below peak_thres_abs~~~~~~~
+    # """
 
-		if scatter_detection:
-			show_detection(image[i],df)
-
-		blob_max = []
-		for blob in df_np:
-			x,y,r = int(blob[1]), int(blob[2]), int(round(blob[3]))
-			patch = image[i][x-r:x+r+1, y-r:y+r+1]
-			max = patch.max()
-			blob_max.append(max)
-
-		df['blob_max'] = blob_max
-		threshold_abs = image[i].max() * peak_threshold_rel
-		df = df[(df['blob_max'] >= threshold_abs)]
-
-		blobs_df = blobs_df.append(df, ignore_index=True)
-
-
+	peak_thres_abs = blobs_df['peak'].max() * peak_thres_rel
 	blobs_df = blobs_df[(blobs_df['x'] - blobs_df['r'] > 0) &
-				  (blobs_df['x'] + blobs_df['r'] + 1 < image[i].shape[0]) &
+				  (blobs_df['x'] + blobs_df['r'] + 1 < frame.shape[0]) &
 				  (blobs_df['y'] - blobs_df['r'] > 0) &
-				  (blobs_df['y'] + blobs_df['r'] + 1 < image[i].shape[1])]
+				  (blobs_df['y'] + blobs_df['r'] + 1 < frame.shape[1]) &
+				  (blobs_df['peak'] > peak_thres_abs)]
 
-	blobs_df['sigma_raw'] = r_to_sigraw*blobs_df['r']
-	blobs_df = blobs_df[['frame','x','y','r', 'sigma_raw']]
+	# """
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~Print detection summary~~~~~~~~~~~~~~~~~~~~~~~~~
+    # """
 
-	return blobs_df
+	if len(blobs_df)==0:
+		print('\n'*3+'#' * 50 \
+                +'\nERROR: No blobs detected in this frame!!!\n' \
+                +'#' * 50+'\n'*3)
+		return pd.DataFrame(np.array([])), np.array([])
+	else:
+		print("Det in frame %d: %s" % (pims_frame.frame_no, len(blobs_df)))
+
+	# """
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Diagnostic~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # """
+
+	plt_array = []
+	if diagnostic:
+	    fig = plt.figure(figsize=(12,9))
+	    ax0 = plt.subplot2grid((6,9),(0,0), colspan=6, rowspan=6)
+
+		# """
+	    # ~~~~~~~~~~~~~~~~~~~~~~~~~~Annotate the blobs~~~~~~~~~~~~~~~~~~~~~~~~~~
+	    # """
+	    ax0.imshow(frame, cmap="gray", aspect='equal')
+	    anno_blob(ax0, blobs_df, marker='^',
+	            plot_r=plot_r, color=(0,0,1,0.8))
+
+		# """
+	    # ~~~~~~~~~~~~~~~~~~~Annotate ground truth if needed~~~~~~~~~~~~~~~~~~~
+	    # """
+	    if isinstance(truth_df, pd.DataFrame):
+	        anno_scatter(ax0, truth_df, marker='o', color=(0,1,0,0.8))
+
+		# """
+	    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~Add scale bar~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	    # """
+	    font = {'family': 'arial', 'weight': 'bold','size': 16}
+	    scalebar = ScaleBar(pixel_size, 'nm', location = 'upper right',
+	        font_properties=font, box_color = 'black', color='white')
+	    scalebar.length_fraction = .3
+	    scalebar.height_fraction = .025
+	    ax0.add_artist(scalebar)
+
+		# """
+	    # ~~~~~~~~~~~~~~~~~Add histograms of several properties~~~~~~~~~~~~~~~~~
+	    # """
+	    ax1 = plt.subplot2grid((6,9),(1,6), colspan=3)
+	    ax1.hist(blobs_df['mass']/blobs_df['mass'].max(),
+	            bins=20, density=1, color=(0,0,0,0.3))
+	    ax1.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+	    ax1.set(xlabel='relative blob intensity mass', ylabel='weight (a.u)')
+	    ax1.set_xlim([0,1])
+	    ax2 = plt.subplot2grid((6,9),(2,6), colspan=3)
+	    ax2.hist(blobs_df['mean']/blobs_df['mean'].max(), bins=20, density=1,
+	               color=(0,0,0,0.3))
+	    ax2.set(xlabel='relative blob intensity mean', ylabel='weight (a.u)')
+	    ax2.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+	    ax2.set_xlim([0,1])
+	    ax3 = plt.subplot2grid((6,9),(3,6), colspan=3)
+	    ax3.hist(blobs_df['std']/blobs_df['std'].max(), bins=20, density=1,
+	               color=(0,0,0,0.3))
+	    ax3.set(xlabel='relative blob intensity std', ylabel='weight (a.u)')
+	    ax3.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+	    ax3.set_xlim([0,1])
+	    ax4 = plt.subplot2grid((6,9),(4,6), colspan=3)
+	    ax4.hist(blobs_df['peak']/blobs_df['peak'].max(), bins=20, density=1,
+	               color=(0,0,0,0.3))
+	    ax4.set(xlabel='relative blob intensity peak', ylabel='weight (a.u)')
+	    ax4.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+	    ax4.set_xlim([0,1])
+
+	    plt_array = plot_end(fig, pltshow)
+
+	return blobs_df, plt_array
 
 
-def show_detection(image, df):
+def detect_blobs_batch(pims_frames,
+			min_sig=1,
+			max_sig=3,
+			num_sig=5,
+			blob_thres=0.1,
+			peak_thres_rel=0.1,
+			r_to_sigraw=3,
+			pixel_size = 108.4,
+			diagnostic=False,
+			pltshow=False,
+			plot_r=True,
+			truth_df=None):
 
-		df_np = df.to_numpy()
-		fig, ax = plt.subplots()
-		ax.imshow(image, cmap='gray')
-		font = {'family': 'arial', 'weight': 'bold','size': 16}
-		scalebar = ScaleBar(1/25, 'um', location = 'upper right', font_properties=font, box_color = 'black', color='white')
-		scalebar.length_fraction = .3
-		scalebar.height_fraction = .025
-		ax.add_artist(scalebar)
-		ax.scatter(df['y'],df['x'], color='red', s = 1)
-		for blob in df_np:
-			frame, y, x, r = blob
-			c = plt.Circle((x, y), r, color='red', linewidth=1, fill=False)
-			ax.add_patch(c)
-		plt.show()
+    """
+    Detect blobs for the whole movie.
+
+    Parameters
+    ----------
+    See detect_blobs().
+
+    Returns
+    -------
+    blobs_df : DataFrame
+        columns = ['frame', 'x', 'y', 'sig_raw', 'r',
+					'peak', 'mass', 'mean', 'std']
+    plt_array :  ndarray
+        ndarray of diagnostic plots.
+
+    Examples
+    --------
+	import pims
+	from cellquantifier.smt.detect import detect_blobs, detect_blobs_batch
+	frames = pims.open('cellquantifier/data/simulated_cell.tif')
+	detect_blobs_batch(frames, diagnostic=0)
+    """
+
+	# """
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Prepare blobs_df~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # """
+
+    columns = ['frame', 'x', 'y', 'sig_raw', 'r', 'peak', 'mass', 'mean', 'std']
+    blobs_df = pd.DataFrame([], columns=columns)
+    plt_array = []
+
+	# """
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Update blobs_df~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # """
+
+    for i in range(len(pims_frames)):
+        current_frame = pims_frames[i]
+        fnum = current_frame.frame_no
+        if isinstance(truth_df, pd.DataFrame):
+            current_truth_df = truth_df[truth_df['frame'] == fnum]
+        else:
+            current_truth_df = None
+
+        tmp, tmp_plt_array = detect_blobs(pims_frames[i],
+                       min_sig=min_sig,
+                       max_sig=max_sig,
+                       num_sig=num_sig,
+                       blob_thres=blob_thres,
+                       peak_thres_rel=peak_thres_rel,
+					   r_to_sigraw=r_to_sigraw,
+					   pixel_size=pixel_size,
+                       diagnostic=diagnostic,
+                       pltshow=pltshow,
+                       plot_r=plot_r,
+                       truth_df=current_truth_df)
+        blobs_df = pd.concat([blobs_df, tmp])
+        plt_array.append(tmp_plt_array)
+
+    blobs_df.index = range(len(blobs_df))
+    plt_array = np.array(plt_array)
+
+    return blobs_df, plt_array
--- cellquantifier/smt/fit_psf.py
+++ cellquantifier/smt/fit_psf.py
@@ -4,14 +4,16 @@ from cellquantifier.math.gaussian_2d import (gaussian_2d, get_moments,
                                             fit_gaussian_2d)
 from cellquantifier.util.plot_util.anno import anno_scatter, anno_blob
 from cellquantifier.util.plot_util._plot_end import plot_end
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 
 def fit_psf(pims_frame,
             blobs_df,
-            diagnostic=False,
-            pltshow=False,
+            diagnostic=True,
+            pltshow=True,
             diag_max_dist_err=1,
-            diag_max_sig_to_sigraw = 2,
+            diag_max_sig_to_sigraw = 3,
             truth_df=None,
             segm_df=None):
     """
@@ -39,49 +41,41 @@ def fit_psf(pims_frame,
     Returns
     -------
     psf_df : DataFrame
-        columns=['frame', 'x_raw', 'y_raw', 'r',
-                'A', 'x', 'y', 'sig_x', 'sig_y', 'phi','area', 'mass',
-                'dist_err', 'sigx_to_sigraw', 'sigy_to_sigraw']
-    plt_array_2d :  2d ndarray
+        columns=['frame', 'x_raw', 'y_raw', 'sig_raw', 'r',
+                'peak', 'mass', 'mean', 'std',
+                'A', 'x', 'y', 'sig_x', 'sig_y', 'phi',
+                'area', 'dist_err', 'sigx_to_sigraw', 'sigy_to_sigraw']
+    plt_array :  2d ndarray
         2D ndarray of diagnostic plot.
 
     Examples
     --------
-    import os as _os
-    import os.path as osp
-    data_dir = osp.abspath(osp.dirname(__file__))
-    fname = _os.path.join(data_dir,
-               'nubo/data/nucleosome.tif')
-
     import pims
-    from nubo.segm import blob_locate
-    frames = pims.open(fname)
-    f, plt_segm = blob_locate(frames[0],
-                    min_sigma=1,
-                    max_sigma=2,
-                    num_sigma=5,
-                    blob_threshold=0.00025,
-                    peak_threshold_rel=0.1,
-                    diagnostic=1,
-                    pltshow=1)
-
-    from nubo.fitt import fit_psf
-    from nubo.io import psf_annotate
-
-    df, plt_fitt = fit_psf(frames[0], f, 1, 1, 1)
+    from cellquantifier.smt.detect import detect_blobs, detect_blobs_batch
+    from cellquantifier.smt.fit_psf import fit_psf, fit_psf_batch
+    frames = pims.open('cellquantifier/data/simulated_cell.tif')
+    blobs_df, det_plt_array = detect_blobs(frames[0], diagnostic=0)
+    psf_df, fit_plt_array = fit_psf(frames[0], blobs_df)
     """
 
     # """
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~Prepare the dataformat~~~~~~~~~~~~~~~~~~~~~~~~~~
     # """
-    df = pd.DataFrame([], columns=['frame', 'x_raw', 'y_raw', 'r',
-            'A', 'x', 'y', 'sig_x', 'sig_y', 'phi',
-            'area', 'mass', 'dist_err', 'sigx_to_sigraw', 'sigy_to_sigraw'])
 
-    df['frame'] = blobs_df['frame']
-    df['x_raw'] = blobs_df['x']
-    df['y_raw'] = blobs_df['y']
-    df['r'] = blobs_df['r']
+    df = pd.DataFrame([], columns=['frame', 'x_raw', 'y_raw', 'r', 'sig_raw',
+            'peak', 'mass', 'mean', 'std',
+            'A', 'x', 'y', 'sig_x', 'sig_y', 'phi',
+            'area', 'dist_err', 'sigx_to_sigraw', 'sigy_to_sigraw'])
+
+    df['frame'] = blobs_df['frame'].to_numpy()
+    df['x_raw'] = blobs_df['x'].to_numpy()
+    df['y_raw'] = blobs_df['y'].to_numpy()
+    df['r'] = blobs_df['r'].to_numpy()
+    df['sig_raw'] = blobs_df['sig_raw'].to_numpy()
+    df['peak'] = blobs_df['peak'].to_numpy()
+    df['mass'] = blobs_df['mass'].to_numpy()
+    df['mean'] = blobs_df['mean'].to_numpy()
+    df['std'] = blobs_df['std'].to_numpy()
 
     # """
     # ~~~~~~~~~~~~~~~~~~~~~~~Fit each blob. If fail, pass~~~~~~~~~~~~~~~~~~~~~~~
@@ -92,10 +86,10 @@ def fit_psf(pims_frame,
         x0 = int(df.at[i, 'x_raw'])
         y0 = int(df.at[i, 'y_raw'])
         delta = int(round(df.at[i, 'r']))
-        Sub_img = pims_frame[x0-delta:x0+delta+1, y0-delta:y0+delta+1]
+        patch = pims_frame[x0-delta:x0+delta+1, y0-delta:y0+delta+1]
 
         try:
-            p, p_err = fit_gaussian_2d(Sub_img)
+            p, p_err = fit_gaussian_2d(patch)
             A = p[0]
             x0_refined = x0 - delta + p[1]
             y0_refined = y0 - delta + p[2]
@@ -110,7 +104,7 @@ def fit_psf(pims_frame,
             df.at[i, 'sig_y'] = sig_y
             df.at[i, 'phi'] = phi
             df.at[i, 'area'] = np.pi * sig_x * sig_y
-            df.at[i, 'mass'] = Sub_img.sum()
+            # df.at[i, 'mass'] = patch.sum()
             df.at[i, 'dist_err'] = ((x0_refined - x0)**2 + \
                             (y0_refined - y0)**2) ** 0.5
             df.at[i, 'sigx_to_sigraw'] = sig_x / sig_raw
@@ -138,19 +132,18 @@ def fit_psf(pims_frame,
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Diagnostic~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # """
 
-    plt_array_2d = []
+    plt_array = []
     if diagnostic:
-        import matplotlib.pyplot as plt
-        from nubo.io import plt2rgbndarray
-        from matplotlib.ticker import FormatStrFormatter
+        # """
+	    # ~~~~~~~~~~~~~~~~~~~~~Print foci num after filters~~~~~~~~~~~~~~~~~~~~~
+	    # """
         f1 = df.copy()
-
         df_filt = pd.DataFrame([], columns=['tot_foci_num'],
-                index=['segm', 'fitt', 'dist_err', 'sigx_to_sigraw',
+                index=['detected', 'fit_success', 'dist_err', 'sigx_to_sigraw',
                         'sigy_to_sigraw'])
-        df_filt.loc['segm'] = len(f1)
+        df_filt.loc['detected'] = len(f1)
         f1 = f1.dropna(how='any')
-        df_filt.loc['fitt'] = len(f1)
+        df_filt.loc['fit_success'] = len(f1)
         f1 = f1[ f1['dist_err']<diag_max_dist_err ]
         df_filt.loc['dist_err'] = len(f1)
         f1 = f1[ f1['sigx_to_sigraw']<diag_max_sig_to_sigraw ]
@@ -159,16 +152,18 @@ def fit_psf(pims_frame,
         df_filt.loc['sigy_to_sigraw'] = len(f1)
         print(df_filt)
 
+        # """
+	    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Show the img~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	    # """
         image = pims_frame
         fig = plt.figure(figsize=(12, 9))
 
         ax0 = plt.subplot2grid((5,5),(0,0), colspan=5, rowspan=5)
-        ax0.imshow(image, cmap="gray", aspect='equal')
+        ax0.imshow(image, cmap="gray")
 
         # """
         # ~~~~~~~~~~~~~~~~~~~Add fitting contour to the image~~~~~~~~~~~~~~~~~~~
         # """
-
         for i in f1.index:
             Fitting_X = np.indices(image.shape)
             p0,p1,p2,p3,p4,p5 = (f1.at[i,'A'], f1.at[i,'x'], f1.at[i,'y'],
@@ -187,7 +182,6 @@ def fit_psf(pims_frame,
         # """
         # ~~~~~~~~~~~~~~~~~Annotate truth_df, segm_df, psf_df~~~~~~~~~~~~~~~~~~~
         # """
-
         anno_blob(ax0, f1, marker='x', plot_r=1, color=(1,0,0,0.8))
 
         if isinstance(segm_df, pd.DataFrame):
@@ -196,19 +190,22 @@ def fit_psf(pims_frame,
         if isinstance(truth_df, pd.DataFrame):
             anno_scatter(ax0, truth_df, marker='o', color=(0,1,0,0.8))
 
+        # """
+        # ~~~~~~~~~~~~~~~~Print predict good fitting foci num~~~~~~~~~~~~~~~~~~
+        # """
         ax0.text(0.95,
                 0.00,
                 """
-                Predict good fitting focai num and ratio: %d, %.2f
+                Predict good fitting foci num and ratio: %d, %.2f
                 """ %(good_fitting_num, good_fitting_num/len(blobs_df)),
                 horizontalalignment='right',
                 verticalalignment='bottom',
                 fontsize = 12,
                 color = (1, 1, 1, 0.8),
                 transform=ax0.transAxes)
-        plt_array_2d = plot_end(fig, pltshow)
+        plt_array = plot_end(fig, pltshow)
 
-    return psf_df, plt_array_2d
+    return psf_df, plt_array
 
 
 def fit_psf_batch(pims_frames,
@@ -216,7 +213,7 @@ def fit_psf_batch(pims_frames,
             diagnostic=False,
             pltshow=False,
             diag_max_dist_err=1,
-            diag_max_sig_to_sigraw = 2,
+            diag_max_sig_to_sigraw = 3,
             truth_df=None,
             segm_df=None):
     """
@@ -224,22 +221,7 @@ def fit_psf_batch(pims_frames,
 
     Parameters
     ----------
-    pims_frames : pims.Frame object
-        Whole movie in the format of pims.Frame.
-    bolbs_df : DataFrame
-        bolb_df with columns of 'x', 'y', 'r', 'frame'.
-    diagnostic : bool, optional
-        If true, print the diagnostic strings.
-    pltshow : bool, optional
-        If true, show diagnostic plot.
-    diag_max_dist_err : float, optional
-        Virtual max_dist_err filter.
-    diag_max_sig_to_sigraw : float, optional
-        Virtual diag_max_sig_to_sigraw filter.
-    truth_df : DataFrame, optional
-        Ground truth DataFrame with columns of 'x', 'y', 'frame'.
-    segm_df : DataFrame, optional
-        Segmentation DataFrame with columns of 'x', 'y', 'frame'.
+    See fit_psf().
 
     Returns
     -------
@@ -247,57 +229,49 @@ def fit_psf_batch(pims_frames,
         columns=['frame', 'x_raw', 'y_raw', 'r',
                 'A', 'x', 'y', 'sig_x', 'sig_y', 'phi','area', 'mass',
                 'dist_err', 'sigx_to_sigraw', 'sigy_to_sigraw']
-    plt_array_3d :  3d ndarray
+    plt_array :  3d ndarray
         3D ndarray of diagnostic plot.
 
     Examples
     --------
-    import os as _os
-    import os.path as osp
-    data_dir = osp.abspath(osp.dirname(__file__))
-    fname = _os.path.join(data_dir,
-               'nubo/data/nucleosome.tif')
-
     import pims
-    from nubo.segm import blob_locate
-    frames = pims.open(fname)
-    f, plt_segm = blob_locate(frames[0],
-                    min_sigma=1,
-                    max_sigma=2,
-                    num_sigma=5,
-                    blob_threshold=0.00025,
-                    peak_threshold_rel=0.1,
-                    diagnostic=1,
-                    pltshow=1)
-
-    from nubo.fitt import fit_psf
-    from nubo.io import psf_annotate
-
-    df, plt_fitt = fit_psf(frames[0], f, 1, 1, 1)
+    from cellquantifier.smt.detect import detect_blobs, detect_blobs_batch
+    from cellquantifier.smt.fit_psf import fit_psf, fit_psf_batch
+    frames = pims.open('cellquantifier/data/simulated_cell.tif')
+    blobs_df, det_plt_array = detect_blobs_batch(frames)
+    psf_df, fit_plt_array = fit_psf_batch(frames, blobs_df)
     """
 
-    columns=['frame', 'x_raw', 'y_raw', 'r',
+    # """
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~Prepare the dataformat~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # """
+
+    df = pd.DataFrame([], columns=['frame', 'x_raw', 'y_raw', 'r', 'sig_raw',
+            'peak', 'mass', 'mean', 'std',
             'A', 'x', 'y', 'sig_x', 'sig_y', 'phi',
-            'area', 'mass', 'dist_err', 'sigx_to_sigraw', 'sigy_to_sigraw']
-    df = pd.DataFrame([], columns=columns)
-    plt_array_3d = []
+            'area', 'dist_err', 'sigx_to_sigraw', 'sigy_to_sigraw'])
+    plt_array = []
+
+    # """
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Update blobs_df~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # """
     for i in range(len(pims_frames)):
         current_frame = pims_frames[i]
         fnum = current_frame.frame_no
-        current_blob = blobs_df[blobs_df.frame == fnum]
+        current_blobs_df = blobs_df[blobs_df['frame'] == fnum]
 
         if isinstance(truth_df, pd.DataFrame):
-            curr_truth_df = truth_df[truth_df.frame == fnum]
+            curr_truth_df = truth_df[truth_df['frame'] == fnum]
         else:
             curr_truth_df = None
 
         if isinstance(segm_df, pd.DataFrame):
-            current_segm_df = segm_df[segm_df.frame == fnum]
+            current_segm_df = segm_df[segm_df['frame'] == fnum]
         else:
             current_segm_df = None
 
-        tmp_psf_df, tmp_plt_array_2d = fit_psf(pims_frame=current_frame,
-                       blobs_df=current_blob,
+        tmp_psf_df, tmp_plt_array = fit_psf(pims_frame=current_frame,
+                       blobs_df=current_blobs_df,
                        diag_max_dist_err=diag_max_dist_err,
                        diag_max_sig_to_sigraw = diag_max_sig_to_sigraw,
                        diagnostic=diagnostic,
@@ -305,9 +279,9 @@ def fit_psf_batch(pims_frames,
                        truth_df=curr_truth_df,
                        segm_df=current_segm_df)
         df = pd.concat([df, tmp_psf_df], sort=False)
-        plt_array_3d.append(tmp_plt_array_2d)
+        plt_array.append(tmp_plt_array)
 
     psf_df = df
-    plt_array_3d = np.array(plt_array_3d)
+    plt_array = np.array(plt_array)
 
-    return psf_df, plt_array_3d
+    return psf_df, plt_array
