b2d8675277a25a2716ab7deacd443a221640f2f3
Clayton <cwseitz@indiana.edu>
2019-11-06 11:09:53 -0500

    updated _plot.py

--- cellquantifier/plot/_plot.py
+++ cellquantifier/plot/_plot.py
@@ -2,20 +2,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def plot_hist(labels, **kwargs):
+def plot_hist(labels=None, **kwargs):
 
 	"""Plot histogram
 
 	Parameters
 	----------
 
-	labels: list of labels for x and y axes
+	kwargs: dict
+		dictionary where keys are plot labels and values are array-like
 
-	kwargs: dictionary where keys are plot labels and values are array-like
+	labels: list, optional
+		list of labels for x,y axes
 
 	Example
 	-------
-	>>>from cellquantifier.plot import plot_d_hist
+	>>>from cellquantifier.plot import plot_hist
 	>>>path1 = 'cellquantifier/data/test_Dalpha.csv'
 	>>>path2 = 'cellquantifier/data/test_Dalpha2.csv'
 	>>>df1 = pd.read_csv(path1, index_col=None, header=0)
@@ -35,11 +37,13 @@ def plot_hist(labels, **kwargs):
 
 		ax.hist(value, bins=30, color=colors[list(kwargs.keys()).index(key)], density=True, label=key)
 		ax.legend(loc='upper right')
-		ax.set(xlabel=labels[0], ylabel=labels[1])
 
 		if type(value) is not np.ndarray:
 			value = value.to_numpy()
 		x.append(value)
+
+	if labels:
+		ax.set(xlabel=labels[0], ylabel=labels[1])
 
 	if len(kwargs) == 2:
 		t,p = t_test(x[0], x[1])
@@ -52,40 +56,45 @@ def plot_hist(labels, **kwargs):
 
 	plt.show()
 
-def scatter_bivariate(path,x,y, fit=False):
+def scatter_bivariate(x,y, labels=None, fit=False):
 
 	"""Generate scatter plot of two variables typically for correlation use
 
 	Parameters
 	----------
 
-	path: the file path to the csv containing the fittData dataframe
-
 	x,y: (N,) array_like
 
-	fit: bool
+	labels: list, optional
+		list of labels for x,y axes
+
+	fit: bool, optional
 		whether or not to perform linear regression
 
 	Example
 	-------
 	>>>from cellquantifier.plot import scatter_bivariate
 	>>>path = 'cellquantifier/data/test_Dalpha.csv'
-	>>>scatter_bivariate(path, 'D', 'alpha', fit=True)
+	>>>df = pd.read_csv(path, index_col=None, header=0)
+	>>>x, y = df['D'], df['alpha']
+	>>>scatter_bivariate(x,y, labels=['D','alpha'], fit=True)
+
 
 	"""
 	from scipy.stats import pearsonr
 	from cellquantifier.math import fit_linear
 
 	fig, ax = plt.subplots()
-	df = pd.read_csv(path, index_col=None, header=0)
 
-	slope, intercept, r, p = fit_linear(df[x],df[y])
+	slope, intercept, r, p = fit_linear(x,y)
 
 	if fit:
-		ax.plot(df[x], intercept + slope*df[x], 'lime', label='fitted line')
+		ax.plot(x, intercept + slope*x, 'lime', label='fitted line')
 
-	ax.scatter(df[x], df[y], label=pearsonr, color='orange', s=10)
-	ax.set(xlabel=x, ylabel=y)
+	if labels:
+		ax.set(xlabel=labels[0], ylabel=labels[1])
+
+	ax.scatter(x, y, label=pearsonr, color='orange', s=10)
 	ax.text(0.75,
 			0.75,
 			r'$\mathbf{\rho}$' + """: % .2f""" %(r),
