246f9f2b6d7b04214e05fc4c994ad4cc7b9bbea9
hualin0123 <hualin_0123@hotmail.com>
2021-01-28 11:36:53 -0500

    add ap_merge_physdf.py, pipe_merge_physdf.py

---
+++ ap_merge_physdf.py
@@ -0,0 +1,25 @@
+"""Part I: CellQuantifier Sequence Control"""
+
+control = [
+'merge_physdf',
+]
+
+"""Part II: CellQuantifier Parameter Settings"""
+
+settings = {
+
+  #GENERAL INFO
+  'Input path': '/home/linhua/Desktop/temp/',
+  'Output path': '/home/linhua/Desktop/temp/',
+  'Processed by:': 'Hua Lin',
+  'Str in filename': 'physData.csv',
+  'Strs not in filename': ['physDataMerged.csv'],
+
+  #MERGE_PHYSDF SETTINGS
+  'Merge mode': 'general',
+
+}
+
+"""Part III: Run CellQuantifier"""
+from cellquantifier.util.pipe_merge_physdf import *
+pipe_batch(settings, control)
