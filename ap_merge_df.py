360906df839e7e8f4a41b9a0b6a7d92aec0c0244
hualin0123 <hualin_0123@hotmail.com>
2021-04-22 12:39:58 -0400

    update merge_df

--- ap_merge_df.py
+++ ap_merge_df.py
@@ -10,7 +10,7 @@ settings = {
 
   #GENERAL INFO
   'Input path': '/home/linhua/Desktop/temp/',
-  'Output path': '/home/linhua/Desktop/temp/',
+  'Output path': '/home/linhua/Desktop/output/',
   'Cols to merge': ['x', 'y', 'frame', 'r'],
 
 }
--- cellquantifier/util/pipe_merge_df.py
+++ cellquantifier/util/pipe_merge_df.py
@@ -16,83 +16,56 @@ class Pipe():
 		self.root_name = root_name
 
 	def merge_df(self):
-		pass
-		# today = str(date.today().strftime("%y%m%d"))
-		#
-		# phys_files = glob.glob(self.settings['Input path'] + '*' + \
-		# 	self.settings['Str in filename'])
-		# for phys_file in phys_files:
-		# 	for exclude_str in self.settings['Strs not in filename']:
-		# 		if exclude_str in phys_file:
-		# 			phys_files.remove(phys_file)
-		#
-		# phys_files = np.array(phys_files)
-		# print("######################################")
-		# print("Total number of physData to be merged: %d" % len(phys_files))
-		# print("######################################")
-		# print(phys_files)
-		#
-		# if len(phys_files) > 1:
-		# 	ind = 1
-		# 	tot = len(phys_files)
-		# 	phys_df = merge_physdfs(phys_files, mode=self.settings['Merge mode'])
-		# else:
-		# 	phys_df = pd.read_csv(phys_files[0])
-		#
-		# print("######################################")
-		# print("Rename particles...")
-		# print("######################################")
-		# phys_df['particle'] = phys_df['raw_data'] + phys_df['particle'].apply(str)
-		# phys_df.round(3).to_csv(self.settings['Output path'] + today + \
-		# 				'-physDataMerged.csv', index=False)
-		#
-		# sys.exit()
+		df_list = np.array(sorted(glob.glob(self.settings['Input path'] + \
+			'*' + self.root_name + '*.csv')))
+		print(df_list)
+
+		if self.settings['Cols to merge']:
+			dfs = []
+			for file in df_list:
+				tmp_df = pd.read_csv(file, index_col=False)
+				tmp_df = tmp_df[self.settings['Cols to merge']]
+				dfs.append(tmp_df)
+			phys_df = pd.concat(dfs, ignore_index=True)
+			print(phys_df)
+		else:
+			phys_df = merge_physdfs(df_list, mode='general')
+			print(phys_df)
+
+		phys_df.round(3).to_csv(self.settings['Output path'] + \
+			self.root_name + '-detData.csv', index=False)
+
 
 
 def get_root_name_list(settings_dict):
 	settings = settings_dict.copy()
 	root_name_list = []
-	path_list = glob.glob(settings['Input path'] + '*')
+	path_list = glob.glob(settings['Input path'] + '*' + '.tif')
 	for path in path_list:
-		root_name_list.append(path)
+		filename = path.split('/')[-1]
+		root_name = filename[:filename.find('.tif')]
+		root_name_list.append(root_name)
 
 	return np.array(sorted(root_name_list))
 
 
 def pipe_batch(settings_dict, control_list):
 
-	root_name_list = get_root_name_list(settings_dict)
+	root_name_list = np.array(sorted(get_root_name_list(settings_dict)))
 	print(root_name_list)
 
-	if settings_dict['Cols to merge']:
-		dfs = []
-		for file in root_name_list:
-			tmp_df = pd.read_csv(file, index_col=False)
-			tmp_df = tmp_df[settings_dict['Cols to merge']]
-			dfs.append(tmp_df)
-		phys_df = pd.concat(dfs, ignore_index=True)
-		print(phys_df)
-	else:
-		phys_df = merge_physdfs(root_name_list, mode='general')
-		print(phys_df)
+	print("######################################")
+	print("Total data num to be processed: %d" % len(root_name_list))
+	print(root_name_list)
+	print("######################################")
 
-	phys_df.round(3).to_csv(settings_dict['Output path'] + \
-					'xxxxxx_xx_xxxx-detData.csv', index=False)
+	ind = 0
+	tot = len(root_name_list)
+	for root_name in root_name_list:
+		ind = ind + 1
+		print("\n")
+		print("Processing (%d/%d): %s" % (ind, tot, root_name))
 
-
-
-	# print("######################################")
-	# print("Total data num to be processed: %d" % len(root_name_list))
-	# print(root_name_list)
-	# print("######################################")
-	#
-	# ind = 0
-	# tot = len(root_name_list)
-	# for root_name in root_name_list:
-	# 	ind = ind + 1
-	# 	print("\n")
-	# 	print("Processing (%d/%d): %s" % (ind, tot, root_name))
-	#
-	# 	pipe = Pipe(settings_dict, control_list, root_name)
-	# 	for func in control_list:
-	# 		getattr(pipe, func)()
+		pipe = Pipe(settings_dict, control_list, root_name)
+		for func in control_list:
+			getattr(pipe, func)()
